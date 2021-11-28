a70e89ed0899d0d18baa759ad314384f4e98ca90
Jaggmort <djeedo@gmail.com>
2021-11-28 11:51:13 +0300

    Таймер на минималках

---
+++ src/timer.js
@@ -0,0 +1,30 @@
+export function set_timer() { //Установка таймера + создание элемента для его отображения
+    let timerElement = document.createElement('div');
+    timerElement.className = 'display-remain-time';
+    timerElement.style.color = 'red';
+    timerElement.style.position = 'absolute';
+    timerElement.style.bottom = 0;
+    timerElement.style.left = 0;
+    document.body.append(timerElement);
+    let result = prompt('На сколько установить таймер?', 30);
+    let remainTime = Date.now() + (result*1000)
+    intervalTimer = setInterval(function(){
+        timeLeft = Math.round((remainTime - Date.now()) / 1000);
+        if(timeLeft < 0){
+          clearInterval(intervalTimer);
+          timerElement.remove();
+          alert('done');
+          return ;
+        }
+        displayTimeLeft(timeLeft);
+      }, 1000);
+}
+
+export function displayTimeLeft (timeLeft){ // отображение таймера
+    let minutes = Math.floor(timeLeft / 60);
+    let seconds = timeLeft % 60;
+    let displayString = `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+    let timerE = document.querySelector('.display-remain-time');
+    timerE.textContent = displayString;
+  }
+
